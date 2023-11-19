5801917c270ca5955a7422ca2d4df87216c034d7
poornasirisha <114747843+poornasirisha@users.noreply.github.com>
2023-11-19 12:05:18 +0530

    Update theme_toggler.js

--- theme_toggler/theme_toggler.js
+++ theme_toggler/theme_toggler.js
@@ -1,10 +1,9 @@
-import React, { createContext, useContext, useState } from 'react';
 
-// Step 1: Create a context for the theme
+import { createContext, useContext, useState } from 'react';
+
 const ThemeContext = createContext();
 
-// Step 2: Create a component that provides the theme
-const ThemeProvider = ({ children }) => {
+export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
@@ -18,26 +17,6 @@ const ThemeProvider = ({ children }) => {
   );
 };
 
-// Step 3: Create a component that uses useContext to access the current theme
-const ThemedComponent = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-
-  return (
-    <div style={{ background: theme === 'light' ? '#fff' : '#333', color: theme === 'light' ? '#333' : '#fff', padding: '20px', textAlign: 'center' }}>
-      <h2>Themed Component</h2>
-      <p>Current Theme: {theme}</p>
-      <button onClick={toggleTheme}>Toggle Theme</button>
-    </div>
-  );
+export const useTheme = () => {
+  return useContext(ThemeContext);
 };
-
-// Step 4: Wrap your application with the ThemeProvider
-const App = () => {
-  return (
-    <ThemeProvider>
-      <ThemedComponent />
-    </ThemeProvider>
-  );
-};
-
-export default App;
