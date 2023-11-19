7a9a74132d45923ceb8a3534c2a98871383c2e66
poornasirisha <114747843+poornasirisha@users.noreply.github.com>
2023-11-19 12:22:10 +0530

    Add files via upload

---
+++ Employee_System/App.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import EmployeeList from './EmployeeList';
+import AddEmployeeForm from './AddEmployeeForm';
+
+const App = () => {
+  return (
+    <div>
+      <EmployeeList />
+      <AddEmployeeForm />
+    </div>
+  );
+};
+
+export default App;
