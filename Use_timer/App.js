54fac7b69d7458bb9bc20844f359f83ee0c423ab
poornasirisha <114747843+poornasirisha@users.noreply.github.com>
2023-11-19 11:37:56 +0530

    Create App.js

---
+++ Use_timer/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimerComponent from './TImerComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TimerComponent />
+    </div>
+  );
+}
+
+export default App;
